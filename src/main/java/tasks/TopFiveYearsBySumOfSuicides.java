6b8210a5cfba7117d4883fb3a1db2d113f93f10d
evakule <evakule15@gmail.com>
2020-12-16 13:44:20 +0200

    Updating method to functional style

--- src/main/java/tasks/TopFiveYearsBySumOfSuicides.java
+++ src/main/java/tasks/TopFiveYearsBySumOfSuicides.java
@@ -5,11 +5,13 @@ import model.SuicideInfoRecord;
 import org.apache.spark.sql.Dataset;
 
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class TopFiveYearsBySumOfSuicides {
 
@@ -24,20 +26,14 @@ public class TopFiveYearsBySumOfSuicides {
                             (oldValue, newValue) -> oldValue, LinkedHashMap::new));
   }
 
-  private Map<String, Integer> getSumOfSuicidesInCountryByYear(
-          Dataset<SuicideInfoRecord> data) {
-    List<CountryYearSuicideAmount> timeAndAmount = getCountryYearSuicideAmountList(data);
-    Map<String, Integer> map = new HashMap<>();
-    for (CountryYearSuicideAmount cys : timeAndAmount) {
-      Integer value = map.get(cys.getCountryYear());
-      if (value != null) {
-        Integer newValue = map.get(cys.getCountryYear()) + cys.getAmountOfSuicideIntTheYear();
-        map.put(cys.getCountryYear(), newValue);
-      } else {
-        map.put(cys.getCountryYear(), cys.getAmountOfSuicideIntTheYear());
-      }
-    }
-    return map;
+  private Map<String, Integer> getSumOfSuicidesInCountryByYear(Dataset<SuicideInfoRecord> data) {
+    return getCountryYearSuicideAmountList(data).stream()
+            .collect(
+                    groupingBy(
+                            CountryYearSuicideAmount::getCountryYear,
+                            summingInt(CountryYearSuicideAmount::getAmountOfSuicideIntTheYear)
+                    )
+            );
   }
 
   private LinkedHashMap<String, Integer> getSortedByValueMap(Map<String, Integer> map) {
